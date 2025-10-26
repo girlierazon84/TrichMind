1e61b1db535cfe0e7aba8c52de9267b1c44e92d7
Girlie Razon <girlie.razon84@gmail.com>
2025-10-26 18:32:03 +0100

    Add summary routes and start weekly summary scheduler in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { connectMongo } from "./config/mongo";
 import { notFound, errorHandler } from "./middlewares/error";
+import { startWeeklySummaryScheduler } from "./utils/scheduler";
+import summaryRoutes from "./routes/summaryRoutes";
 import { logger } from "./utils/logger";
 import authRoutes from "./routes/authRoutes";
 import alertRoutes from "./routes/alertRoutes";
@@ -16,10 +18,14 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/alerts", alertRoutes);
+app.use("/api/summary", summaryRoutes);
 
 // 404 + Error Handlers
 app.use(notFound);
 app.use(errorHandler);
+
+// Start scheduler
+startWeeklySummaryScheduler();
 
 // Start server
 connectMongo().then(() => {
