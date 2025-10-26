a03aceecf0bf88edda75cd42821da690548d9009
Girlie Razon <girlie.razon84@gmail.com>
2025-10-26 19:38:25 +0100

    Add index file to export models for user management and gameplay tracking

---
+++ server/src/models/index.ts
@@ -0,0 +1,7 @@
+export { default as User } from "./User";
+export { default as HealthLog } from "./HealthLog";
+export { default as JournalEntry } from "./JournalEntry";
+export { default as Trigger } from "./TriggersInsights";
+export { default as GameSession } from "./TrichGame";
+export { default as TrichBotMessage } from "./TrichBot";
+export { default as Predict } from "./Predict";
