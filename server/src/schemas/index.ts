4a09d683960feb5a919a09b870233db2dd291fe3
Girlie Razon <girlie.razon84@gmail.com>
2025-10-26 19:27:09 +0100

    Add index file to export all schema modules

---
+++ server/src/schemas/index.ts
@@ -0,0 +1,8 @@
+// server/src/schemas/index.ts
+export * from "./userSchema";
+export * from "./healthSchema";
+export * from "./journalSchema";
+export * from "./triggersInsightsSchema";
+export * from "./trichGameSchema";
+export * from "./trichBotSchema";
+export * from "./predictSchema";
