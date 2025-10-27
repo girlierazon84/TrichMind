4358a36772dd003fe7082002ede86208fe0b88eb
Girlie Razon <girlie.razon84@gmail.com>
2025-10-27 13:42:42 +0100

    feat(app): restructure App component to include routing and theme provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { MOTIF } from "./theme";
 import { GlobalStyle } from "../src/utils/global/styling/GlobalStyle";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegistrationPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        TrichMind
-      </header>
+      <ThemeProvider theme={MOTIF}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
     </div>
   );
 }
