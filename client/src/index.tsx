b6dbd9ac92983a4e64ced852fd797d6355deac34
Girlie Razon <girlie.razon84@gmail.com>
2025-10-27 13:42:36 +0100

    refactor(index): remove routing and theme provider for simplified structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
-import { ThemeProvider } from "styled-components";
-import { MOTIF } from "./theme";
 import { GlobalStyle } from "../src/utils/global/styling/GlobalStyle";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegistrationPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -16,17 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={MOTIF}>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+      <App />
   </React.StrictMode>
 );
 
