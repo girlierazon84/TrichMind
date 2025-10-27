0dd38ccfcc95c158bffc152782c1866d29ce5629
Girlie Razon <girlie.razon84@gmail.com>
2025-10-27 13:26:26 +0100

    feat(theme): add default theme configuration for styled-components

---
+++ client/src/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+export const MOTIF: DefaultTheme = {
+    primary: "#21b2ba",
+    secondary: "#31becc",
+    thirdly: "#add6da",
+    fourthly: "#c2d1d7",
+    fifthly: "#0d6275",
+    sixthly: "#e0f7f9",
+    page_bg: "#c9e3e4",
+    card_bg: "#fbffff",
+    card_shadow: "0px 2px 6px rgba(0,0,0,0.08)",
+    text_primary: "#2c2e2e",
+    text_secondary: "#797979",
+    high_risk: "#ff0004",
+    high_risk_gradient: "linear-gradient(135deg, #ff0004 0%, #ff6467 100%)",
+    medium_risk: "#f76e19",
+    medium_risk_gradient: "linear-gradient(135deg, #f76e19 0%, #fbb56a 100%)",
+    low_risk: "#22c55e",
+    low_risk_gradient: "linear-gradient(135deg, #22c55e 0%, #a4e3b8 100%)",
+};
