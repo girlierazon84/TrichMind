75e338ef922d4728a195092effe0e9c96b7bac75
Girlie Razon <girlie.razon84@gmail.com>
2025-10-29 16:35:53 +0100

    feat: add index file to export API modules for better organization

---
+++ client/src/api/index.ts
@@ -0,0 +1,14 @@
+// client/src/api/index.ts
+
+export * from "./axiosClient";
+export * from "./authApi";
+export * from "./mlApi";
+export * from "./userApi";
+export * from "./healthApi";
+export * from "./journalApi";
+export * from "./triggersInsightsApi";
+export * from "./trichBotApi";
+export * from "./trichGameApi";
+export * from "./alertApi";
+export * from "./summaryApi";
+export * from "./loggerApi";
