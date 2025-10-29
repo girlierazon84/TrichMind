78e68a720a4f33f44740a7579441a0de80d5fad2
Girlie Razon <girlie.razon84@gmail.com>
2025-10-29 16:41:27 +0100

    chore: update index.ts to include additional log models and improve file path comment

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,3 +1,5 @@
+// server/src/models/index.ts
+
 export { default as User } from "./User";
 export { default as HealthLog } from "./HealthLog";
 export { default as JournalEntry } from "./JournalEntry";
@@ -5,3 +7,6 @@ export { default as Trigger } from "./TriggersInsights";
 export { default as GameSession } from "./TrichGame";
 export { default as TrichBotMessage } from "./TrichBot";
 export { default as Predict } from "./Predict";
+export { default as LogEvent } from "./LogModel";
+export { default as AlertLog } from "./AlertLog";
+export { default as SummaryLog } from "./SummaryLog";
