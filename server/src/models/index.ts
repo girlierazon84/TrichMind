a1dd7a97522010a39bb422b16fa82e376f70141b
Girlie Razon <girlie.razon84@gmail.com>
2025-10-30 19:47:56 +0100

    fix: change exports in index.ts to named exports for consistency

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,12 +1,12 @@
 // server/src/models/index.ts
 
 export { User } from "./User";
-export { default as HealthLog } from "./HealthLog";
-export { default as JournalEntry } from "./JournalEntry";
-export { default as Trigger } from "./TriggersInsights";
-export { default as GameSession } from "./TrichGame";
-export { default as TrichBotMessage } from "./TrichBot";
-export { default as Predict } from "./Predict";
-export { default as LogEvent } from "./LogModel";
-export { default as AlertLog } from "./AlertLog";
-export { default as SummaryLog } from "./SummaryLog";
+export { HealthLog } from "./HealthLog";
+export { JournalEntry } from "./JournalEntry";
+export { Trigger } from "./TriggersInsights";
+export { GameSession } from "./TrichGame";
+export { TrichBotMessage } from "./TrichBot";
+export { Predict } from "./Predict";
+export { LogEvent } from "./LogModel";
+export { AlertLog } from "./AlertLog";
+export { SummaryLog } from "./SummaryLog";
