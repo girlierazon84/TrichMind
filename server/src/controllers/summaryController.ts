9a59b65592cbd065553ed48dc1f8d7a97827edec
Girlie Razon <girlie.razon84@gmail.com>
2025-10-30 19:47:10 +0100

    fix: update import statement for SummaryLog in summaryController

--- server/src/controllers/summaryController.ts
+++ server/src/controllers/summaryController.ts
@@ -1,7 +1,7 @@
 // server/src/controllers/summaryController.ts
 import { Request, Response } from "express";
 import { User } from "../models/User";
-import SummaryLog from "../models/SummaryLog";
+import { SummaryLog } from "../models/SummaryLog";
 import { sendMail } from "../utils/mailer";
 import { buildWeeklySummaryEmail } from "../templates/weeklySummaryEmail";
 import { logger } from "../utils/logger";
