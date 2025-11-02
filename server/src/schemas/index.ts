ffeb62ac3855c49e7795905293eee31fb5ed4463
Girlie Razon <girlie.razon84@gmail.com>
2025-11-02 23:30:03 +0100

    feat: add index file to export all schemas

---
+++ server/src/schemas/index.ts
@@ -0,0 +1,9 @@
+// server/src/schemas/index.ts
+
+export * from "./userSchema";
+export * from "./healthSchema";
+export * from "./journalSchema";
+export * from "./triggersInsightsSchema";
+export * from "./trichGameSchema";
+export * from "./trichBotSchema";
+export * from "./predictSchema";
