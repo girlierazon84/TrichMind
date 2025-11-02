c3a3ed83e4b3ed5b961b632e7f82db10bb68a2c6
Girlie Razon <girlie.razon84@gmail.com>
2025-11-03 00:12:02 +0100

    feat: create index file for model exports

---
+++ server/src/models/index.ts
@@ -0,0 +1,12 @@
+// server/src/models/index.ts
+
+export { User } from "./UserModel";
+export { HealthLog } from "./HealthLog";
+export { JournalEntry } from "./JournalEntry";
+export { Trigger } from "./TriggersInsightsModel";
+export { GameSession } from "./TrichGameModel";
+export { TrichBotMessage } from "./TrichBotModel";
+export { Predict } from "./PredictModel";
+export { LogEvent } from "./LogModel";
+export { AlertLog } from "./AlertLog";
+export { SummaryLog } from "./SummaryLog";
