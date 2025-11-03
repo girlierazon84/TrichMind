3d6f40c33c2d04b7454ca1d5d1fb8816e9d93b73
Girlie Razon <girlie.razon84@gmail.com>
2025-11-03 01:16:57 +0100

    feat: add index file to export all service modules

---
+++ server/src/services/index.ts
@@ -0,0 +1,12 @@
+// server/src/services/index.ts
+
+export * from "./userService";
+export * from "./healthService";
+export * from "./journalService";
+export * from "./triggersInsightsService";
+export * from "./trichGameService";
+export * from "./trichBotService";
+export * from "./predictService";
+export * from "./alertService";
+export * from "./summaryService";
+export * from "./loggerService";
