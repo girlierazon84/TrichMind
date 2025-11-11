edc59ed99cd442e5e0fb8c6dfe7e340b57c6e9c3
Girlie Razon <girlie.razon84@gmail.com>
2025-11-11 02:43:35 +0100

    Refactor import statement in trichBotApi for improved organization

--- client/src/services/trichBotApi.ts
+++ client/src/services/trichBotApi.ts
@@ -1,6 +1,6 @@
 // client/src/services/trichBotApi.ts
 
-import { axiosClient } from "./axiosClient";
+import { axiosClient } from "@/services";
 import { withLogging } from "@/utils/withLogging";
 
 
