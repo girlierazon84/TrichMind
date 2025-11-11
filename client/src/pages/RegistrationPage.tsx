561ea08058cbd29a8459cb84ca7801bb41cf37b4
Girlie Razon <girlie.razon84@gmail.com>
2025-11-11 18:49:50 +0100

    Add RegistrationPage component for user account creation

---
+++ client/src/pages/RegistrationPage.tsx
@@ -0,0 +1,20 @@
+// client/src/pages/RegistrationPage.tsx
+
+import React from "react";
+import styled from "styled-components";
+import { RegisterPredictForm } from "@/components/RegisterPredictForm.tsx";
+
+
+const RegistrationContainer = styled.main`
+  padding: 2rem;
+  background: ${({ theme }) => theme.colors.page_bg || "#c9e3e4"};
+`;
+
+export default function RegistrationPage() {
+  return (
+    <RegistrationContainer>
+      <h1>Create Your Account</h1>
+      <RegisterPredictForm />
+    </RegistrationContainer>
+  );
+}
