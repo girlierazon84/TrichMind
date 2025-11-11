44ed810e60e4ae0b26d32f6ac3ad227fd85b0215
Girlie Razon <girlie.razon84@gmail.com>
2025-11-11 02:22:29 +0100

    Add central index file for exporting all React hooks

---
+++ client/src/hooks/index.ts
@@ -0,0 +1,18 @@
+// client/src/hooks/index.ts
+
+// Central barrel for all React hooks
+
+export * from "./useAuth";
+export * from "./usePredict";
+export * from "./useRegisterAndPredict";
+
+export * from "./useUser";
+export * from "./useLogger";
+
+export * from "./useAlerts";
+export * from "./useHealth";
+export * from "./useJournal";
+export * from "./useSummary";
+export * from "./useTrichBot";
+export * from "./useTrichGame";
+export * from "./useTriggers";
