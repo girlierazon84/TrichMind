a1b3cda3bf3f2d7c45b3923aafec1834e3222a2d
Girlie Razon <girlie.razon84@gmail.com>
2025-11-11 02:28:21 +0100

    Refactor import statement in useLogger hook for improved organization

--- client/src/hooks/useLogger.ts
+++ client/src/hooks/useLogger.ts
@@ -2,7 +2,7 @@
 
 import { useCallback } from "react";
 import { toast } from "react-toastify"; // ✅ you'll need `npm i react-toastify`
-import { loggerApi, type LogEvent} from "@/services/loggerApi";
+import { loggerApi, type LogEvent} from "@/services";
 
 
 /**
