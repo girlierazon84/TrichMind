51b71fc7485d53d13f085f948715b40c8b76e848
Girlie Razon <girlie.razon84@gmail.com>
2025-11-11 21:27:48 +0100

    Add index file to export components for easier imports

---
+++ client/src/components/index.ts
@@ -0,0 +1,12 @@
+// client/src/components/index.ts
+
+export { BottomNav } from './BottomNav';
+export { CopingStrategiesCard } from './CopingStrategiesCard';
+export { DailyProgressCard } from './DailyProgressCard';
+export { FormInput } from "./FormInput";
+export { ProtectedRoute } from "./ProtectedRoute";
+export { RegisterPredictForm } from "./RegisterPredictForm";
+export { ResultCard } from "./ResultCard";
+export { RiskResultCard } from "./RiskResultCard";
+export { RiskTrendChart } from "./RiskTrendChart";
+export { ThemeButton } from "./ThemeButton";
