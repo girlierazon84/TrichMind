75fd6d9a6f7289aa96301590f5001f30df16d0b5
Girlie Razon <girlie.razon84@gmail.com>
2025-11-12 01:10:49 +0100

    Remove inline padding from DailyProgressCard component for cleaner styling

--- client/src/components/DailyProgressCard.tsx
+++ client/src/components/DailyProgressCard.tsx
@@ -1,6 +1,6 @@
 // client/src/components/DailyProgressCard.tsx
 export const DailyProgressCard = () => {
-    return <div style={{ padding: "2rem" }}>📈 Daily Progress</div>;
+    return <div>📈 Daily Progress</div>;
 }
 
 export default DailyProgressCard;
