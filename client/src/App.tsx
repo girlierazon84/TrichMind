2733ccfee027fd01cfc52a74f2f930b20a91e7a6
Girlie Razon <girlie.razon84@gmail.com>
2025-11-13 00:21:53 +0100

    Add spacing for improved readability in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 // client/src/App.tsx
+
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyle } from "@/styles";
 import { AppRoutes } from "@/routes/AppRoutes";
+
 
 export const App = () => (
   <ThemeProvider theme={theme}>
