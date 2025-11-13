c634c41032a56c9d8a6b572f189d41c0a266054e
Girlie Razon <girlie.razon84@gmail.com>
2025-11-13 17:33:07 +0100

    Refactor export statements in index.ts for improved organization

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,12 +1,12 @@
 // server/src/models/index.ts
 
-export { User } from "./UserModel";
-export { HealthLog } from "./HealthLog";
-export { JournalEntry } from "./JournalEntry";
-export { Trigger } from "./TriggersInsightsModel";
-export { GameSession } from "./TrichGameModel";
-export { TrichBotMessage } from "./TrichBotModel";
-export { Predict } from "./PredictModel";
-export { LogEvent } from "./LogModel";
-export { AlertLog } from "./AlertLog";
-export { SummaryLog } from "./SummaryLog";
+export * from "./AlertLog";
+export * from "./HealthLog";
+export * from "./JournalEntry";
+export * from "./LogModel";
+export * from "./PredictModel";
+export * from "./SummaryLog";
+export * from "./TrichBotModel";
+export * from "./TrichGameModel";
+export * from "./TriggersInsightsModel";
+export * from "./UserModel";
