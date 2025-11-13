e17591d5bd6a71e40cd38a3276c9806c6ab2b61e
Girlie Razon <girlie.razon84@gmail.com>
2025-11-13 17:02:54 +0100

    Add index.ts to export all controller modules for improved organization

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,11 @@
+// server/src/controllers/index.ts
+
+export * from "./alertController";
+export * from "./authController";
+export * from "./healthController";
+export * from "./journalController";
+export * from "./predictController";
+export * from "./summaryController";
+export * from "./trichBotController";
+export * from "./trichGameController";
+export * from "./triggersInsightsController";
