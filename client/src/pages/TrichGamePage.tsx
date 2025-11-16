26353f6de335c25e1f843cd5df9bb49ad08e85ac
Girlie Razon <girlie.razon84@gmail.com>
2025-11-16 15:51:00 +0100

    feat(TrichGamePage): add TrichGamePage component with initial structure

---
+++ client/src/pages/TrichGamePage.tsx
@@ -0,0 +1,12 @@
+// client/src/pages/TrichGamePage.tsx
+
+
+export const TrichGamePage = () => {
+    return (
+        <div>
+            <h1>TrichGame Page</h1>
+        </div>
+    );
+}
+
+export default TrichGamePage;
