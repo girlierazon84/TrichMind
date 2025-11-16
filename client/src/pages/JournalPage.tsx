85d9f1b43514e2ea6526c640c9c6b4d401d0cb7f
Girlie Razon <girlie.razon84@gmail.com>
2025-11-16 15:50:45 +0100

    feat(JournalPage): add JournalPage component with basic structure

---
+++ client/src/pages/JournalPage.tsx
@@ -0,0 +1,12 @@
+// client/src/pages/JournalPage.tsx
+
+
+export const JournalPage = () => {
+    return (
+        <div>
+            <h1>Journal Page</h1>
+        </div>
+    );
+}
+
+export default JournalPage;
