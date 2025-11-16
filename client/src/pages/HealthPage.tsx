c2e7815bfd6ac010cd74f0707ed2d77f780f8538
Girlie Razon <girlie.razon84@gmail.com>
2025-11-16 15:50:38 +0100

    feat(HealthPage): create HealthPage component with basic structure

---
+++ client/src/pages/HealthPage.tsx
@@ -0,0 +1,12 @@
+// client/src/pages/HealthPage.tsx
+
+
+export const HealthPage = () => {
+    return (
+        <div>
+            <h1>Health Page</h1>
+        </div>
+    );
+}
+
+export default HealthPage;
