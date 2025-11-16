e5b19bddf6b03b50d8ea044fcb2cd842bccde195
Girlie Razon <girlie.razon84@gmail.com>
2025-11-16 15:51:06 +0100

    feat(TriggersInsightsPage): create TriggersInsightsPage component with initial structure

---
+++ client/src/pages/TriggersInsightsPage.tsx
@@ -0,0 +1,12 @@
+// client/src/pages/TriggersInsightsPage.tsx
+
+
+export const TriggersInsightsPage = () => {
+    return (
+        <div>
+            <h1>Triggers & Insights Page</h1>
+        </div>
+    );
+}
+
+export default TriggersInsightsPage;
