2ce1fa0da40407ffeae69711fb45a2c3c4c33b85
Girlie Razon <girlie.razon84@gmail.com>
2025-11-17 02:14:26 +0100

    refactor(main): update AppRoutes import to use named export for consistency

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "@/routes/AppRoutes";
+import { AppRoutes } from "./routes";
 
 
 // Render the main application
