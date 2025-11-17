b53f09b59f7ce4a213831c5c792072dcd49949ea
Girlie Razon <girlie.razon84@gmail.com>
2025-11-17 02:55:40 +0100

    feat(types): add custom Request interface to include user information

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,11 @@
+// server/src/types/express.d.ts
+
+import { User } from "@prisma/client";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: { id: string };
+        }
+    }
+}
