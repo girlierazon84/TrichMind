a5ed5f91b18dbc2e628ad4259bafde0187eb895a
Girlie Razon <girlie.razon84@gmail.com>
2025-11-17 02:26:13 +0100

    refactor(main): restructure main entry point to include ThemeProvider and GlobalStyle

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
+import { ThemeProvider } from "styled-components";
+import { theme, GlobalStyle } from "@/styles";  // your theme file
+import { AppRoutes } from "@/routes";
 
 
-// Render the main application
+// Render the application
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <AppRoutes />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <BrowserRouter>
+                <AppRoutes />
+            </BrowserRouter>
+        </ThemeProvider>
     </React.StrictMode>
 );
