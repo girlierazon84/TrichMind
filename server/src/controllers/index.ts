897ce3a7dde34234bfb64ff7167af8296af9e5ea
Girlie Razon <girlie.razon84@gmail.com>
2025-11-18 16:19:14 +0100

    feat(controller): add getRiskTrendController export to index

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -9,4 +9,4 @@ export * from "./summaryController";
 export * from "./trichBotController";
 export * from "./trichGameController";
 export * from "./triggersInsightsController";
-export * from "./healthTrend";
+export * from "./getRiskTrendController";
