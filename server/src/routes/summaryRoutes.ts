20d32b6a7b96ccb3a0f0af4b814b839d3852a79e
Girlie Razon <girlie.razon84@gmail.com>
2025-11-23 00:23:35 +0100

    fix(summaryRoutes): improve formatting for route definition

--- server/src/routes/summaryRoutes.ts
+++ server/src/routes/summaryRoutes.ts
@@ -4,11 +4,14 @@ import { Router } from "express";
 import { sendWeeklySummaries } from "../controllers";
 import { authentication } from "../middlewares";
 
-
 // Initialize router
 const router = Router();
 
 // 🔹 Manual trigger (for admin/testing)
-router.post("/weekly", authentication({ required: false }), sendWeeklySummaries);
+router.post(
+    "/weekly",
+    authentication({ required: false }),
+    sendWeeklySummaries
+);
 
 export default router;
