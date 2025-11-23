872fb01af51724318f41bfd827c9a4dc424fb0b3
Girlie Razon <girlie.razon84@gmail.com>
2025-11-23 03:53:36 +0100

    fix(PublicRoute): enhance comments formatting for clarity and consistency

--- client/src/routes/PublicRoute.tsx
+++ client/src/routes/PublicRoute.tsx
@@ -1,16 +1,19 @@
 // client/src/routes/PublicRoute.tsx
 
+import type React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks";
 
-
-// A wrapper for <Route> that redirects to the home screen if you're authenticated.
+/**-------------------------------------------------------------------------------------
+    A wrapper for <Route> that redirects to the home screen if you're authenticated.
+----------------------------------------------------------------------------------------*/
 export const PublicRoute = ({ children }: { children: React.ReactElement }) => {
     const { isAuthenticated, loading } = useAuth();
 
     if (loading) return <div>Loading...</div>;
-
     if (isAuthenticated) return <Navigate to="/" replace />;
 
     return children;
 };
+
+export default PublicRoute;
