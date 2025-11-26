932d1ca459ea9064dc8a42257c8f5ed6545136f4
Girlie Razon <girlie.razon84@gmail.com>
2025-11-27 00:50:11 +0100

    refactor(routes): reorganize route imports and mounting order for clarity

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,27 +1,37 @@
 // server/src/routes/index.ts
 
 import { Router } from "express";
+
+import alertRoutes from "./alertRoutes";
 import authRoutes from "./authRoutes";
-import userRoutes from "./userRoutes";
 import healthRoutes from "./healthRoutes";
 import journalRoutes from "./journalRoutes";
-import triggersInsightsRoutes from "./triggersInsightsRoutes";
-import trichGameRoutes from "./trichGameRoutes";
-import trichBotRoutes from "./trichBotRoutes";
+import loggerRoutes from "./loggerRoutes";
 import predictRoutes from "./predictRoutes";
+import relapseOverviewRoutes from "./relapseOverviewRoutes";
+import summaryRoutes from "./summaryRoutes";
+import trichBotRoutes from "./trichBotRoutes";
+import trichGameRoutes from "./trichGameRoutes";
+import triggersInsightsRoutes from "./triggersInsightsRoutes";
+import userRoutes from "./userRoutes";
+
 
 
 // Main router that combines all route modules
 const router = Router();
 
 // Mounting individual route modules
+router.use("/alerts", alertRoutes);
 router.use("/auth", authRoutes);
-router.use("/user", userRoutes);
 router.use("/health", healthRoutes);
 router.use("/journal", journalRoutes);
-router.use("/triggers", triggersInsightsRoutes);
-router.use("/game", trichGameRoutes);
-router.use("/bot", trichBotRoutes);
+router.use("/logs", loggerRoutes);
 router.use("/predict", predictRoutes);
+router.use("/relapse-overview", relapseOverviewRoutes);
+router.use("/summary", summaryRoutes);
+router.use("/bot", trichBotRoutes);
+router.use("/game", trichGameRoutes);
+router.use("/triggers", triggersInsightsRoutes);
+router.use("/user", userRoutes);
 
 export default router;
