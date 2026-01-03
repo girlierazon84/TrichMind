65a20f51b674cfb642ffddbcced3f85b2fefb210
Girlie Razon <girlie.razon84@gmail.com>
2026-01-03 16:54:13 +0100

    feat(hooks): add barrel exports for client hooks Create client/src/hooks/index.ts with "use client" Re-export auth, prediction, user, logging, alerts, health, journal, summary, TrichBot/game, triggers, and sobriety/relapse hooks

---
+++ client/src/hooks/index.ts
@@ -0,0 +1,23 @@
+// client/src/hooks/index.ts
+"use client";
+
+export * from "./useAuth";
+export * from "./usePredict";
+export * from "./useRegisterAndPredict";
+
+export * from "./useSoberStreak";
+export * from "./useCopingStrategies";
+export * from "./useRiskTrendChart";
+export * from "./usePredictedRiskTrend";
+export * from "./useRelapseOverview";
+
+export * from "./useUser";
+export * from "./useLogger";
+
+export * from "./useAlerts";
+export * from "./useHealth";
+export * from "./useJournal";
+export * from "./useSummary";
+export * from "./useTrichBot";
+export * from "./useTrichGame";
+export * from "./useTriggers";
