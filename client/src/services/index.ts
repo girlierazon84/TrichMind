060e04355f4de47df04f433f48f80e98e0199162
Girlie Razon <girlie.razon84@gmail.com>
2026-01-03 16:32:18 +0100

    chore(client): add services barrel exports for API modules

---
+++ client/src/services/index.ts
@@ -0,0 +1,17 @@
+// client/src/services/index.ts
+"use client";
+
+export * from "./axiosClient";
+
+export * from "./authApi";
+export * from "./predictApi";
+export * from "./userApi";
+export * from "./loggerApi";
+export * from "./alertApi";
+export * from "./healthApi";
+export * from "./journalApi";
+export * from "./summaryApi";
+export * from "./trichBotApi";
+export * from "./trichGameApi";
+export * from "./triggersInsightsApi";
+export * from "./relapseOverviewApi";
