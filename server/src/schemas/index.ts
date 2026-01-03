c000854761e718a15ef1197f5cb84dedb6ee4efc
Girlie Razon <girlie.razon84@gmail.com>
2026-01-03 03:03:56 +0100

    chore(schemas): add barrel export for schema modules

---
+++ server/src/schemas/index.ts
@@ -0,0 +1,9 @@
+// server/src/schemas/index.ts
+
+export * from "./healthSchema";
+export * from "./journalSchema";
+export * from "./predictSchema";
+export * from "./trichBotSchema";
+export * from "./trichGameSchema";
+export * from "./triggersInsightsSchema";
+export * from "./userSchema";
