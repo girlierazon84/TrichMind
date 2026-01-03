a63e0b3bad04f535a67a273a0ebe4a995eec71a5
Girlie Razon <girlie.razon84@gmail.com>
2026-01-03 03:35:37 +0100

    chore(controllers): export all controllers from index barrel

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,14 @@
+// server/src/controllers/index.ts
+
+export * from "./alertController";
+export * from "./authController";
+export * from "./healthController";
+export * from "./journalController";
+export * from "./predictController";
+export * from "./summaryController";
+export * from "./trichBotController";
+export * from "./trichGameController";
+export * from "./triggersInsightsController";
+export * from "./getRiskTrendController";
+export * from "./relapseOverviewController";
+export * from "./userProfile.controller";
