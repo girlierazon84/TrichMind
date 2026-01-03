090aa4df9965acf145b38c3e7f99af274247490e
Girlie Razon <girlie.razon84@gmail.com>
2026-01-03 15:43:15 +0100

    add icons to client assets

---
+++ client/src/assets/icons/index.ts
@@ -0,0 +1,19 @@
+// client/src/assets/icons/index.ts
+
+export { default as CheckMarkIcon } from "./check.png";
+export { default as CalendarClockIcon } from "./datetime.png";
+export { default as ArrowEnterIcon } from "./enter.png";
+export { default as XFailedIcon } from "./failed.png";
+export { default as HealthIcon } from "./health.png";
+export { default as HomeIcon } from "./home.png";
+export { default as InsightsIcon } from "./insights.png";
+export { default as JournalIcon } from "./journal.png";
+export { default as MyJournalIcon } from "./my_journal.png";
+export { default as SaveIcon } from "./save.png";
+export { default as TrichBotIcon } from "./trichbot.png";
+export { default as TrichGameIcon } from "./trichgame.png";
+export { default as TriggersInsightsIcon } from "./triggers.png";
+export { default as UserIcon } from "./user.png";
+export { default as BrainIcon } from "./brain.png";
+export { default as BackIcon } from "./previous.png";
+export { default as StrategyIcon } from "./strategies.png";
