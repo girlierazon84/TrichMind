8fdb10cf1b27ce3b54ad82f04ddb147661f93fd5
Girlie Razon <girlie.razon84@gmail.com>
2026-01-03 02:44:29 +0100

    chore(templates): export all email templates from index

---
+++ server/src/templates/index.ts
@@ -0,0 +1,7 @@
+// server/src/templates/index.ts
+
+export * from "./baseEmail";
+export * from "./relapseAlertEmail";
+export * from "./resetPasswordEmail";
+export * from "./weeklySummaryEmail";
+export * from "./welcomeEmail";
