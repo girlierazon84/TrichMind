78a6875d659c26e351a94fdedda8f9d3fed77f88
Girlie Razon <girlie.razon84@gmail.com>
2026-01-03 15:08:10 +0100

    feat(services): export all service modules from index barrel

---
+++ server/src/services/index.ts
@@ -0,0 +1,14 @@
+// server/src/services/index.ts
+
+export * from "./userService";
+export * from "./healthService";
+export * from "./journalService";
+export * from "./triggersInsightsService";
+export * from "./trichGameService";
+export * from "./trichBotService";
+export * from "./predictService";
+export * from "./alertService";
+export * from "./summaryService";
+export * from "./loggerService";
+export * from "./relapseOverview.service";
+export * from "./mlClient";
