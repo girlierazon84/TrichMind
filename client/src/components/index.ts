bdd663b19dea0ee0e30f928cb85e0fdaabb4fef5
Girlie Razon <girlie.razon84@gmail.com>
2026-01-03 17:13:23 +0100

    chore(components): re-export UI components from central index Exports BottomNav, cards, form inputs, risk result/trend components, and ThemeButton via client/src/components/index.ts.

---
+++ client/src/components/index.ts
@@ -0,0 +1,10 @@
+// client/src/components/index.ts
+
+export * from "./BottomNav";
+export * from "./CopingStrategiesCard";
+export * from "./DailyProgressCard";
+export * from "./FormInput";
+export * from "./RegisterPredictForm";
+export * from "./RiskResultCard";
+export * from "./RiskTrendChart";
+export * from "./ThemeButton";
