f6a4663499255559243f6406e3aca94a569d487a
Girlie Razon <girlie.razon84@gmail.com>
2026-01-04 22:12:25 +0100

    refactor(login): simplify login page wrapper to render client component directly

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,18 +1,7 @@
 // client/src/app/(auth)/login/page.tsx
 
-import { Suspense } from "react";
-import styled from "styled-components";
 import LoginClient from "./LoginClient";
 
-
-const LoadingFallback = styled.div`
-    padding: 24px;
-`;
-
 export default function LoginPage() {
-    return (
-        <Suspense fallback={<LoadingFallback>Loading…</LoadingFallback>}>
-            <LoginClient />
-        </Suspense>
-    );
+    return <LoginClient />;
 }
